f7c58f1a2abf16ffb1adeb7ad490862ebd410fda
Hirotaka Yoneda <squar37@gmail.com>
2018-10-15 17:56:47 +0900

    Minor fix at geometry.cpp

--- geometry.cpp
+++ geometry.cpp
@@ -15,10 +15,10 @@ public:
 	point2d operator+(const point2d& p) const { return point2d(*this) += p; }
 	point2d operator-(const point2d& p) const { return point2d(*this) -= p; }
 	point2d operator*(const double v) const { return point2d(*this) *= v; }
-	double norm() { return x * x + y * y; }
-	double abs() { return std::sqrt(x * x + y * y); }
-	double dot(const point2d& p) { return x * p.x + y * p.y; }
-	double cross(const point2d& p) { return x * p.y - y * p.x; }
+	double norm() const { return x * x + y * y; }
+	double abs() const { return std::sqrt(x * x + y * y); }
+	double dot(const point2d& p) const { return x * p.x + y * p.y; }
+	double cross(const point2d& p) const { return x * p.y - y * p.x; }
 	int ccw(const point2d& p1, const point2d& p2) {
 		// RETURNS: P0 -> P1 -> P2 IS COLLINEAR --> ((P0, P1, P2) IS CENTER --> (2, -2, 0)), COUNTER-CLOCKWISE --> 1, CLOCKWISE --> -1
 		point2d pr1 = p1 - point2d(*this);
@@ -33,37 +33,38 @@ public:
 	point2d p1, p2;
 	segment2d() : p1(point2d()), p2(point2d()) {};
 	segment2d(const point2d& p1_, const point2d& p2_) : p1(p1_), p2(p2_) {};
-	double norm() { return (p1 - p2).norm(); }
-	double abs() { return (p1 - p2).abs(); }
+	double norm() const { return (p1 - p2).norm(); }
+	double abs() const { return (p1 - p2).abs(); }
 };
-bool intersect(segment2d& s1, segment2d& s2) {
+
+bool intersect(const segment2d& s1, const segment2d& s2) {
 	return s1.p1.ccw(s1.p2, s2.p1) * s1.p1.ccw(s1.p2, s2.p2) <= 0 && s2.p1.ccw(s2.p2, s1.p1) * s2.p1.ccw(s2.p2, s1.p2) <= 0;
 }
-point2d intersect_point(segment2d &s1, segment2d &s2) {
+point2d intersect_point(const segment2d &s1, const segment2d &s2) {
 	point2d base = s2.p2 - s2.p1;
 	double d1 = std::abs(base.cross(s1.p1 - s2.p1));
 	double d2 = std::abs(base.cross(s1.p2 - s2.p1));
 	return s1.p1 + (s1.p2 - s1.p1) * (d1 / (d1 + d2));
 }
-double distance(point2d& p, segment2d& s) {
+double distance(const point2d& p, const segment2d& s) {
 	double d1 = (p - s.p1).norm();
 	double d2 = (p - s.p2).norm();
 	double d0 = s.norm();
 	if (d0 + d1 < d2 || d0 + d2 < d1) return std::sqrt(std::min(d1, d2));
 	return std::abs((p - s.p1).cross(s.p2 - s.p1)) / std::sqrt(d0);
 }
-double distance(segment2d& s1, segment2d& s2) {
+double distance(const segment2d& s1, const segment2d& s2) {
 	if (intersect(s1, s2)) return 0.0;
 	return std::min({ distance(s1.p1, s2), distance(s1.p2, s2), distance(s2.p1, s1), distance(s2.p2, s1) });
 }
-double area(std::vector<point2d> v) {
+double area(const std::vector<point2d>& v) {
 	double sum = 0.0;
 	for (int i = 0; i < v.size(); ++i) {
 		sum += v[i].cross(v[(i + 1) % v.size()]);
 	}
 	return std::abs(sum) * 0.5;
 }
-int contain(std::vector<point2d> v, point2d p) {
+int contain(const std::vector<point2d>& v, point2d p) {
 	// (IN POLYGON, ON THE EDGE, OUT OF POLYGON) = (1, 0, -1)
 	int ans = -1;
 	for (int i = 0; i < v.size(); ++i) {
@@ -75,7 +76,7 @@ int contain(std::vector<point2d> v, point2d p) {
 	}
 	return ans;
 }
-std::vector<int> convex_hull(std::vector<point2d> v) {
+std::vector<int> convex_hull(const std::vector<point2d>& v) {
 	int N = v.size();
 	std::vector<int> perm(N);
 	for (int i = 0; i < N; ++i) perm[i] = i;
