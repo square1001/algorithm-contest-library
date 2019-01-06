8f6699295b687c5283dc86bc0b6c3289d30f381a
Hirotaka Yoneda <squar37@gmail.com>
2019-01-06 19:41:15 +0900

    Created modint library

---
+++ modint.cpp
@@ -0,0 +1,27 @@
+template<uint32_t mod>
+class modint {
+private:
+	uint32_t n;
+public:
+	modint() : n(0) {};
+	modint(uint64_t n_) : n(n_ % mod) {};
+	bool operator==(const modint& m) const { return n == m.n; }
+	bool operator!=(const modint& m) const { return n != m.n; }
+	uint32_t get() const { return n; }
+	modint& operator+=(const modint& m) { n += m.n; n = (n < mod ? n : n - mod); return *this; }
+	modint& operator-=(const modint& m) { n += mod - m.n; n = (n < mod ? n : n - mod); return *this; }
+	modint& operator*=(const modint& m) { n = uint64_t(n) * m.n % mod; return *this; }
+	modint operator+(const modint& m) const { return modint(*this) += m; }
+	modint operator-(const modint& m) const { return modint(*this) -= m; }
+	modint operator*(const modint& m) const { return modint(*this) *= m; }
+	modint binpow(uint64_t b) const {
+		modint ans = 1, m = modint(*this);
+		while (b) {
+			if (b & 1) ans *= m;
+			m *= m;
+			b >>= 1;
+		}
+		return ans;
+	}
+	modint inv() { return (*this).binpow(mod - 2); }
+};
